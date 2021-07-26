968fd75283d974a39f9a85a29445ada1c9410065
Javier Soriano <javiersoriano@users.noreply.github.com>
2021-07-26 13:05:54 +0200

    adding imDns rule and cisco umbrella events

--- Artifacts/Scripts/IngestCSV.ps1
+++ Artifacts/Scripts/IngestCSV.ps1
@@ -180,4 +180,8 @@ $status = SendToLogA -url "https://raw.githubusercontent.com/javiersoriano/senti
 
 Write-Host $status
 
+$status = SendToLogA -url "https://raw.githubusercontent.com/javiersoriano/sentinel-training/main/Artifacts/Telemetry/model_evasion_detection_CL_alerts.csv" -EventsTable "Cisco_Umbrella_dns"
+
+Write-Host $status
+
 Start-Sleep -Seconds 540
